665029f72f8a87d05b6fbcb4ee15655e91331b4f
unknown <marija.nikolova@students.finki.ukim.mk>
2019-03-28 11:10:57 +0100

    main part implemented

--- program.cpp
+++ program.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+
 #include <cstring>
 using namespace std;
 
+
 void printMyName () {
-	cout << "Marija Nikolova" << endl;
+	
+cout << "Marija Nikolova" << endl;
+
 }
+
 
 void printMyIndex () {
-	cout << "163084 " << endl;
+	
+cout << "163084 " << endl;
+
 }
 
+
 int main () {
-	//TODO
+	
+printMyName();
+printMyIndex();
 }
